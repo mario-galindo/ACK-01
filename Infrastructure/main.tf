c5aa9f6be107ad6cc7f333bbe9884b4f47764d64
mario-galindo <mgalindo9410@gmail.com>
2021-07-21 16:36:53 -0600

    cidr_blocks defined to assign them to a vpc and subnets

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -9,6 +9,16 @@ variable "private_key_path" {}
 variable "region" {}
 variable "environment_tag" {}
 
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+variable "subnet2_address_space" {
+  default = "10.1.1.0/24"
+}
+
 ############
 # PROVIDERS
 ############
