f1a720f020c99cc36782c7e3c8a1014df74b261d
mario-galindo <mgalindo9410@gmail.com>
2021-07-21 16:56:26 -0600

    Basic networking resources created: vpc, subnets and internet gateway for the vpc

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -49,3 +49,35 @@ data "aws_availability_zones" "available" {}
 # RESOURCES
 ############
 
+# Networking #
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.network_address_space
+  enable_dns_hostnames = "true"
+
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-vpc" })
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-igw" })
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block              = var.subnet1_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-subnet-1" })
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block              = var.subnet2_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-subnet-2" })
+}
+
