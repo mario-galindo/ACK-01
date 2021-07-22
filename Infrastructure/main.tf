31ba5a962afd7313685138426b6ee52b3eea4919
mario-galindo <mgalindo9410@gmail.com>
2021-07-21 21:52:34 -0600

    Routing added to the network components

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -81,3 +81,25 @@ resource "aws_subnet" "subnet2" {
   tags = merge(local.common_tags, { Name = "${var.environment_tag}-subnet-2" })
 }
 
+# Routing #
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-rtb" })
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+resource "aws_route_table_association" "rta-subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rtb.id
+}
+
